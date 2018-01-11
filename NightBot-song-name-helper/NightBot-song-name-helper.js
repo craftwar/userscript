026b4505f9953b8f518006e0cfdab357406d0852
craftwar <craftwar@users.noreply.github.com>
2018-01-11 16:37:10 +0800

    Add NightBot-song-name-helper

---
+++ NightBot-song-name-helper/NightBot-song-name-helper.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @author craftwar
+// @name NightBot-AutoDJ song-name helper
+// @description NightBot-AutoDJ song name helper for craftwar's obs plugin (http://craftwarblog.blogspot.com/2018/01/obs-plugin-playing-song.html)
+// @namespace github.com.craftwar
+// @match https://beta.nightbot.tv/song_requests
+// @grant none
+// @run-at document-end
+// ==/UserScript==
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+sleep(3000).then(() => {
+  element = document.querySelector(".current-track > h4:nth-child(1) > strong:nth-child(1)");
+  setInterval( ()=> {document.title = element.childNodes[0].wholeText + " - YouTube";}, 1000);
+// cross-origin is not allowed, use timer to update
+
+/*
+var iframe = document.getElementsByTagName('iframe');
+var innerDoc = iframe[0].contentDocument || iframe[0].contentWindow.document;
+var videoelement = document.getElementsByTagName('video');
+*/
+})

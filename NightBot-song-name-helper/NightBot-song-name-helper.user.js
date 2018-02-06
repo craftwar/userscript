5d595a34f021704edd016c6b4cf6b34bfafbaeaa
craftwar <craftwar@users.noreply.github.com>
2018-02-06 15:54:30 +0800

    NightBot-song-name-helper: Use MutationObserver to detect change
    better than setInterval check

--- NightBot-song-name-helper/NightBot-song-name-helper.user.js
+++ NightBot-song-name-helper/NightBot-song-name-helper.user.js
@@ -5,22 +5,29 @@
 // @copyright 2018, craftwar (https://craftwarblog.blogspot.com/)
 // @license GPL-3.0+; http://www.gnu.org/licenses/gpl-3.0.txt
 // @homepageURL https://github.com/craftwar/userscript/tree/master/NightBot-song-name-helper
-// @version 0.1.20180113
+// @version 0.1.20180206
 // @namespace github.com.craftwar
 // @match https://beta.nightbot.tv/song_requests
 // @grant none
 // @run-at document-end
 // ==/UserScript==
 
+// increase wait_time if it's not working
+const wait_time=5000;
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-sleep(3000).then(() => {
-  element = document.querySelector(".current-track > h4:nth-child(1) > strong:nth-child(1)");
-  setInterval( ()=> {document.title = element.childNodes[0].wholeText + " - YouTube";}, 1000);
-// cross-origin is not allowed, use timer to update
+sleep(wait_time).then(() => {
+  element = document.querySelector(".current-track > h4:first-child > strong:first-child").childNodes[0];
+  document.title = element.wholeText + " - YouTube";
 
+  let observer = new MutationObserver( ()=> {
+    document.title = element.wholeText + " - YouTube";
+  });
+  observer.observe(element, { characterData: true });
+  
+// cross-origin is not allowed, use timer to update
 /*
 var iframe = document.getElementsByTagName('iframe');
 var innerDoc = iframe[0].contentDocument || iframe[0].contentWindow.document;
---
+++ NightBot-song-name-helper/README.md
@@ -0,0 +1,3 @@
+This script can be installed from  
+https://greasyfork.org/scripts/37311-nightbot-autodj-song-name-helper  
+https://openuserjs.org/scripts/craftwar/NightBot-AutoDJ_song-name_helper

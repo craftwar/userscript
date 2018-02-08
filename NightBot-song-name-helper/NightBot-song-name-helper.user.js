73e600f603935786dac1a0aa4f932508aabd9e9c
craftwar <craftwar@users.noreply.github.com>
2018-02-09 00:02:24 +0800

    NightBot-song-name-helper: Use MutationObserver to replace unreliable sleep

--- NightBot-song-name-helper/NightBot-song-name-helper.user.js
+++ NightBot-song-name-helper/NightBot-song-name-helper.user.js
@@ -5,32 +5,25 @@
 // @copyright 2018, craftwar (https://craftwarblog.blogspot.com/)
 // @license GPL-3.0+; http://www.gnu.org/licenses/gpl-3.0.txt
 // @homepageURL https://github.com/craftwar/userscript/tree/master/NightBot-song-name-helper
-// @version 0.1.20180206
+// @version 0.1.20180208
 // @namespace github.com.craftwar
-// @match https://beta.nightbot.tv/song_requests
+// @match https://beta.nightbot.tv/song_requests*
 // @grant none
 // @run-at document-end
 // ==/UserScript==
 
-// increase wait_time if it's not working
-const wait_time=5000;
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
+let body_element = document.querySelector("body");
 
-sleep(wait_time).then(() => {
-  element = document.querySelector(".current-track > h4:first-child > strong:first-child").childNodes[0];
-  document.title = element.wholeText + " - YouTube";
+let body_observer = new MutationObserver( (record, observer)=> {
+	let element = document.querySelector(".current-track > h4:first-child > strong:first-child").childNodes[0];
+	if (element) {
+		observer.disconnect();
 
-  let observer = new MutationObserver( ()=> {
-    document.title = element.wholeText + " - YouTube";
-  });
-  observer.observe(element, { characterData: true });
-  
-// cross-origin is not allowed, use timer to update
-/*
-var iframe = document.getElementsByTagName('iframe');
-var innerDoc = iframe[0].contentDocument || iframe[0].contentWindow.document;
-var videoelement = document.getElementsByTagName('video');
-*/
-})
+		let song_observer = new MutationObserver( ()=> {
+			document.title = element.wholeText + " - YouTube";
+		});
+		song_observer.observe(element, { characterData: true });
+	}
+});
+
+body_observer.observe(body_element, { subtree: true, childList: true });

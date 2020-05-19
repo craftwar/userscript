87279ab340ca1db06de62b6f8e9873208f040213
craftwar <craftwar@users.noreply.github.com>
2020-05-19 23:38:23 +0800

    Add Niconico support

---
+++ Niconico-song-name-helper/Niconico-song-name-helper.user.js
@@ -0,0 +1,36 @@
+// ==UserScript==
+// @author craftwar
+// @name Niconico song-name helper
+// @description Make craftwar's obs plugin (https://craftwarblog.blogspot.com/2018/01/obs-plugin-playing-song.html) able to get Niconico song name.
+// @copyright 2020, craftwar (https://craftwarblog.blogspot.com/)
+// @license GPL-3.0-or-later; https://www.gnu.org/licenses/gpl-3.0.txt
+// @homepageURL https://github.com/craftwar/userscript/tree/master/Spotify-song-name-helper
+// @version 0.1.20200520
+// @namespace github.com.craftwar
+// @match https://www.nicovideo.jp/*
+// @grant none
+// @run-at document-end
+// @inject-into content
+// ==/UserScript==
+// inject-into content for Violentmonkey, default mode page is not working
+// ref: https://violentmonkey.github.io/2018/11/23/inject-into-context/
+
+(() => {
+	const suffixes = [" - ニコニコ動画", " - Niconico Video", " - niconico動畫"];
+	const fake_suffix = " - YouTube";
+	const fix_suffix = () => {
+		if (!document.title.endsWith(fake_suffix)) {
+			for (const suffix of suffixes) {
+				if (document.title.endsWith(suffix)) {
+					document.title = document.title.substring(0, document.title.length - suffix.length) + fake_suffix;
+					break;
+				}
+			}
+		}
+	}
+	fix_suffix();
+	const track_observer = new MutationObserver(fix_suffix);
+
+	const title_element = document.querySelector("title");
+	track_observer.observe(title_element, { childList: true });
+})();
---
+++ Niconico-song-name-helper/README.md
@@ -0,0 +1,3 @@
+This script can be installed from  
+https://greasyfork.org/scripts/403749-niconico-song-name-helper  
+https://openuserjs.org/scripts/craftwar/Niconico_song-name_helper

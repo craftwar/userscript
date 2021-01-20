128750b95fa6ca25a3ac5703d1d1423cff29f8ac
craftwar <craftwar@users.noreply.github.com>
2021-01-20 20:09:08 +0800

    Niconico: Fix potential title change fail
    may happen rarely

--- Niconico-song-name-helper/Niconico-song-name-helper.user.js
+++ Niconico-song-name-helper/Niconico-song-name-helper.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @author craftwar
 // @name Niconico song-name helper
-// @description Make craftwar's obs plugin (https://craftwarblog.blogspot.com/2018/01/obs-plugin-playing-song.html) able to get Niconico song name.
-// @copyright 2020, craftwar (https://craftwarblog.blogspot.com/)
+// @description Make craftwar's obs plugin (https://craftwarblog.blogspot.com/2018/01/obs-plugin-playing-song.html) able to get song name.
+// @copyright 2020-2021, craftwar (https://craftwarblog.blogspot.com/)
 // @license GPL-3.0-or-later; https://www.gnu.org/licenses/gpl-3.0.txt
 // @homepageURL https://github.com/craftwar/userscript/tree/master/Spotify-song-name-helper
-// @version 0.1.20201005
+// @version 0.1.20210120
 // @namespace github.com.craftwar
 // @match https://www.nicovideo.jp/*
 // @grant none
@@ -29,9 +29,9 @@
 			}
 		}
 	}
-	fix_suffix();
-	const track_observer = new MutationObserver(fix_suffix);
 
 	const title_element = document.querySelector("title");
+	const track_observer = new MutationObserver(fix_suffix);
 	track_observer.observe(title_element, { childList: true });
+	fix_suffix();
 })();

899fdde115736a5ca2113b6c9aef868c70e2f714
craftwar <craftwar@users.noreply.github.com>
2022-08-30 22:06:43 +0800

    Spotify-song-name-helper: Reduce code

--- Spotify-song-name-helper/Spotify-song-name-helper.user.js
+++ Spotify-song-name-helper/Spotify-song-name-helper.user.js
@@ -2,10 +2,10 @@
 // @author craftwar
 // @name Spotify song-name helper
 // @description Make craftwar's obs plugin (https://craftwarblog.blogspot.com/2018/01/obs-plugin-playing-song.html) able to get Spotify song name.
-// @copyright 2020, craftwar (https://craftwarblog.blogspot.com/)
+// @copyright 2022, craftwar (https://craftwarblog.blogspot.com/)
 // @license GPL-3.0-or-later; https://www.gnu.org/licenses/gpl-3.0.txt
 // @homepageURL https://github.com/craftwar/userscript/tree/master/Spotify-song-name-helper
-// @version 0.1.20201005
+// @version 0.1.20220830
 // @namespace github.com.craftwar
 // @match https://open.spotify.com/*
 // @grant none
@@ -17,11 +17,9 @@
 
 'use strict';
 (() => {
-const track_observer = new MutationObserver( ()=> {
-	if (!document.title.endsWith(" - YouTube"))
-		document.title += " - YouTube";
-});
-
-const title_element = document.querySelector("title");
-track_observer.observe(title_element, {childList: true});
+	const title_element = document.querySelector("title");
+	new MutationObserver(() => {
+		if (!document.title.endsWith(" - YouTube"))
+			document.title += " - YouTube";
+	}).observe(title_element, { childList: true });
 })();

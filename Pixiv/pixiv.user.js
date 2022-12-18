a0c41ef81acc769502f05defccfd222ff695313f
craftwar <craftwar@users.noreply.github.com>
2022-12-19 00:18:56 +0800

    Pixiv: Add KeyR to toggle auto-click show all behavior

--- Pixiv/pixiv.user.js
+++ Pixiv/pixiv.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @author craftwar
-// @name pixiv click show all
+// @name pixiv auto-click show all
 // @namespace github.com.craftwar
-// @version 0.1.20221002
+// @version 0.1.202212.18
 // @copyright 2022, craftwar (https://craftwarblog.blogspot.com/)
 // @license GPL-3.0-or-later; https://www.gnu.org/licenses/gpl-3.0.txt
 // @match https://www.pixiv.net/*
@@ -12,9 +12,25 @@
 
 'use strict';
 (() => {
+	let showAll = true;
+	window.addEventListener("keydown", (event) => {
+		if (event.code == "KeyR") {
+			showAll = !showAll;
+			if (!showAll) {
+				var exitShowAllButton = document.body.querySelector("div.gtm-manga-viewer-close-icon");
+				if (exitShowAllButton)
+					exitShowAllButton.click();
+			} else
+				clickShowAll();
+		}
+	});
+	function clickShowAll() {
+		const showAllButton = document.body.querySelector("button.sc-emr523-0");
+		if (showAllButton)
+			showAllButton.click();
+	}
 	new MutationObserver((record, observer) => {
-		const showAll = document.body.querySelector("button.sc-emr523-0");
 		if (showAll)
-			showAll.click();
+			clickShowAll();
 	}).observe(document.body, { subtree: true, childList: true });
 })();

bd31e2ca085430e49f52221ecdc52da83526d616
craftwar <craftwar@users.noreply.github.com>
2023-05-01 19:03:22 +0800

    Dictionary: Add MediaPlayPause hotkey to play voice

--- Dictionary/dictionary auto pronounce.user.js
+++ Dictionary/dictionary auto pronounce.user.js
@@ -2,10 +2,10 @@
 // @author craftwar
 // @name Dictionary auto pronounce
 // @description Automatically pronounce searched word. You need manaully add website to autoplay whitelist to prevent error "Uncaught (in promise) DOMException: play() failed because the user didn't interact with the document first."
-// @copyright 2022, craftwar (https://craftwarblog.blogspot.com/)
+// @copyright 2023, craftwar (https://craftwarblog.blogspot.com/)
 // @license GPL-3.0-or-later; https://www.gnu.org/licenses/gpl-3.0.txt
 // @homepageURL https://github.com/craftwar/userscript/tree/master/Dictionary
-// @version 0.1.20220829
+// @version 0.1.20230501
 // @namespace github.com.craftwar
 // @match https://www.dictionary.com/*
 // @match https://tw.dictionary.search.yahoo.com/*
@@ -14,11 +14,21 @@
 // @run-at document-idle
 // ==/UserScript==
 
-document.addEventListener('readystatechange', () => {
-  if (document.readyState == "complete") {
-    if (window.location.href.startsWith("https://dictionary.cambridge.org/"))
-      document.getElementsByTagName('audio')[1].play();
-    else
-      document.getElementsByTagName('audio')[0].play();
-  }
-})
+'use strict';
+(() => {
+  let voice;
+  document.addEventListener('readystatechange', () => {
+    if (document.readyState == "complete") {
+      if (window.location.href.startsWith("https://dictionary.cambridge.org/"))
+        voice = document.getElementsByTagName('audio')[1];
+      else
+        voice = document.getElementsByTagName('audio')[0];
+      if (voice)
+        voice.play();
+    }
+  });
+  window.addEventListener("keydown", (event) => {
+    if (event.key == "MediaPlayPause")
+      voice.play();
+  });
+})();

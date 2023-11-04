7346c0898656a50261d873aa0356b03b6949325a
craftwar <craftwar@users.noreply.github.com>
2023-11-05 03:43:49 +0800

    Pixiv: Add skip confirmation of url jump

--- Pixiv/pixiv.user.js
+++ Pixiv/pixiv.user.js
@@ -1,9 +1,10 @@
 // ==UserScript==
 // @author craftwar
-// @name pixiv auto-click show all
+// @name tweak pixiv
 // @namespace github.com.craftwar
-// @version 0.1.202212.18
-// @copyright 2022, craftwar (https://craftwarblog.blogspot.com/)
+// @description skip confirmation of url jump, press "R" to show all pictures
+// @version 2023.11.05
+// @copyright 2023, craftwar (https://craftwarblog.blogspot.com/)
 // @license GPL-3.0-or-later; https://www.gnu.org/licenses/gpl-3.0.txt
 // @match https://www.pixiv.net/*
 // @grant none
@@ -12,25 +13,42 @@
 
 'use strict';
 (() => {
-	let showAll = true;
-	window.addEventListener("keydown", (event) => {
-		if (event.code == "KeyR") {
-			showAll = !showAll;
-			if (!showAll) {
-				var exitShowAllButton = document.body.querySelector("div.gtm-manga-viewer-close-icon");
-				if (exitShowAllButton)
-					exitShowAllButton.click();
-			} else
-				clickShowAll();
+	if (window.location.href.startsWith("https://www.pixiv.net/jump.php")) {
+		observer = new MutationObserver((record, observer) => {
+			if (skipConfirmation())
+				observer.disconnect();
+		}).observe(document.body, { subtree: true, childList: true, attributes: true });
+		if (skipConfirmation())
+			observer.disconnect();
+		function skipConfirmation() {
+			let link = document.body.querySelector("a")
+			if (link) {
+				link.click();
+				return true;
+			}
 		}
-	});
-	function clickShowAll() {
-		const showAllButton = document.body.querySelector("button.sc-emr523-0");
-		if (showAllButton)
-			showAllButton.click();
 	}
-	new MutationObserver((record, observer) => {
-		if (showAll)
-			clickShowAll();
-	}).observe(document.body, { subtree: true, childList: true });
+	else {
+		let showAll = false;
+		window.addEventListener("keydown", (event) => {
+			if (event.code == "KeyR") {
+				showAll = !showAll;
+				if (!showAll) {
+					var exitShowAllButton = document.body.querySelector("div.gtm-manga-viewer-close-icon");
+					if (exitShowAllButton)
+						exitShowAllButton.click();
+				} else
+					clickShowAll();
+			}
+		});
+		function clickShowAll() {
+			const showAllButton = document.body.querySelector("button.sc-emr523-0");
+			if (showAllButton)
+				showAllButton.click();
+		}
+		// new MutationObserver((record, observer) => {
+		// 	if (showAll)
+		// 		clickShowAll();
+		// }).observe(document.body, { subtree: true, childList: true });
+	}
 })();

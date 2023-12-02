a6a4b075036e99948555ae96800533ca902230f1
craftwar <craftwar@users.noreply.github.com>
2023-12-02 10:26:36 +0800

    Pixiv: Fix auto-click show all button partially
    Press R/ESC key to toggle comic open/close
    This userscript should be implemented as browser extension. (some
    fucntions are not availble)

--- Pixiv/pixiv.user.js
+++ Pixiv/pixiv.user.js
@@ -2,19 +2,21 @@
 // @author craftwar
 // @name tweak pixiv
 // @namespace github.com.craftwar
-// @description skip confirmation of url jump, press "R" to show all pictures
-// @version 2023.11.05
+// @description skip confirmation of url jump, auto-click show all button
+// @version 2023.12.02
 // @copyright 2023, craftwar (https://craftwarblog.blogspot.com/)
 // @license GPL-3.0-or-later; https://www.gnu.org/licenses/gpl-3.0.txt
 // @match https://www.pixiv.net/*
+// @noframes
 // @grant none
+// @inject-into content
 // @run-at document-idle
 // ==/UserScript==
 
 'use strict';
 (() => {
 	if (window.location.href.startsWith("https://www.pixiv.net/jump.php")) {
-		observer = new MutationObserver((record, observer) => {
+		const observer = new MutationObserver((record, observer) => {
 			if (skipConfirmation())
 				observer.disconnect();
 		}).observe(document.body, { subtree: true, childList: true, attributes: true });
@@ -27,28 +29,35 @@
 				return true;
 			}
 		}
-	}
-	else {
-		let showAll = false;
+	} else {
+		// press "R" or "ESC" to toggle comic show all
+		var showAll = true;
 		window.addEventListener("keydown", (event) => {
-			if (event.code == "KeyR") {
+			if (event.code == "KeyR" || event.code == "Escape") {
 				showAll = !showAll;
 				if (!showAll) {
-					var exitShowAllButton = document.body.querySelector("div.gtm-manga-viewer-close-icon");
-					if (exitShowAllButton)
+					const exitShowAllButton = document.body.querySelector("div.gtm-manga-viewer-close-icon");
+					if (exitShowAllButton) {
 						exitShowAllButton.click();
-				} else
+					}
+				} else {
 					clickShowAll();
+				}
 			}
 		});
+
 		function clickShowAll() {
 			const showAllButton = document.body.querySelector("button.sc-emr523-0");
-			if (showAllButton)
+			if (showAllButton) {
 				showAllButton.click();
+				return true;
+			}
 		}
-		// new MutationObserver((record, observer) => {
-		// 	if (showAll)
-		// 		clickShowAll();
-		// }).observe(document.body, { subtree: true, childList: true });
+
+		const showAllObserver = new MutationObserver((record, observer) => {
+			if (clickShowAll())
+				observer.disconnect();
+		}).observe(document.body, { subtree: true, childList: true, attributes: true });
+
 	}
 })();

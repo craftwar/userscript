3fad84d81e59cfba24c6b21d0845b93773aa9f4d
craftwar <craftwar@users.noreply.github.com>
2024-01-07 15:39:11 +0800

    AlwaysDesktop: Add https://m.gamer.com.tw/home/creationDetail.php? support

--- AlwaysDesktop/gamer.com.tw.user.js
+++ AlwaysDesktop/gamer.com.tw.user.js
@@ -1,16 +1,17 @@
 // ==UserScript==
 // @author	craftwar
 // @name 	Always desktop bahamut
+// @name:zh-TW 巴哈姆特重導到桌面版網站
 // @description	Redirect mobile bahamut to desktop
 // @description:zh-TW	重導到巴哈姆特桌面版網站
 // @namespace github.com.craftwar
-// @include	https://m.gamer.com.tw/gnn/*
-// @version	23.12.24
+// @include	https://m.gamer.com.tw/*
+// @version	24.1.7
 // @grant	none
 // ==/UserScript==
 'use strict';
 (() => {
-	const m = /^https:\/\/m\.gamer\.com\.tw\/gnn\/(.+)/.exec(window.location.href);
+	const m = /^https:\/\/m\.gamer\.com\.tw\/((?:gnn)|(?:home))\/(.+)/.exec(window.location.href);
 	if (m)
-		window.location.replace("https://gnn.gamer.com.tw/" + m[1]);
+		window.location.replace("https://" + m[1] + ".gamer.com.tw/" + m[2]);
 })();
